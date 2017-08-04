f5afbaf4d3fe87f8a68088b63da577fbcc24bb17
Dylan Lewis <dyldlewis@gmail.com>
2017-08-04 14:18:18 -0700

    create callback function to display doctors

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,10 +1,16 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var displayDoctors = function (doctors) {
+  for(i = 0; i < doctors.length; i++) {
+    $("#results").append("<li>" + doctors[i] + "</li>");
+  }
+}
+
 $(document).ready(function() {
   $("#find_doctor").submit(function(event) {
     event.preventDefault();
     var symptom = $("#symptom").val();
     newDoctor = new Doctor("roger");
-    newDoctor.getDoctors(symptom);
+    newDoctor.getDoctors(symptom, displayDoctors);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -5,13 +5,15 @@ this.name = name;
 }
 
 
-Doctor.prototype.getDoctors = function(symptom) {
+Doctor.prototype.getDoctors = function(symptom, displayDoctors) {
+  var doctors = [];
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ symptom+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-      console.log(symptom)
-      console.log(apiKey);
-      console.log(result.data[0].profile.bio);
-
+      console.log(result);
+      for (i = 0; i < result.data.length; i++) {
+        doctors.push(result.data[i].profile.bio)
+      }
+      displayDoctors(doctors)
     })
    .fail(function(error){
       console.log("fail");
